bf78b616409894076eb73dfd0246ed27ccb09969
chinluchen <chinlu0322@gmail.com>
2025-10-13 10:48:40 +0800

    Initial Commit

---
+++ OptFight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OptFight
+//
+//  Created by 陳慶儒 on 2025/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OptFight/OptFightApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptFightApp.swift
+//  OptFight
+//
+//  Created by 陳慶儒 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct OptFightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
