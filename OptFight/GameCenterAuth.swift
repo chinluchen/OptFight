d931e505d3c50141fb1dd75a5f1269023d6b6603
chinluchen <chinlu0322@gmail.com>
2025-10-13 12:05:32 +0800

    new project, all set up, can be tested.

---
+++ OptFight/GameCenterAuth.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  OptFight
+//
+//  Created by 陳慶儒 on 2025/10/13.
+//
+
---
+++ OptFight/MatchService.swift
@@ -0,0 +1,8 @@
+//
+//  MatchService.swift
+//  OptFight
+//
+//  Created by 陳慶儒 on 2025/10/13.
+//
+
+import Foundation
